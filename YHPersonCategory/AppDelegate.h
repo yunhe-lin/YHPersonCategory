1b7565db652207cd598dd404cd5f3ca7af0b2546
林云鹤 <1367159949@qq.com>
2016-04-27 15:43:17 +0800

    init

---
+++ YHPersonCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  waterFlowLayout
+//
+//  Created by bigMan on 16/4/11.
+//  Copyright © 2016年 bigMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
